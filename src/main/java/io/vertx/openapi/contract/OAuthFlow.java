38900b1609b46147337ecb575114d9a954ac3d3d
Paulo Lopes <pmlopes@gmail.com>
2023-03-22 14:16:30 +0100

    Type safe security scheme
    Signed-off-by: Paulo Lopes <pmlopes@gmail.com>

---
+++ src/main/java/io/vertx/openapi/contract/OAuthFlow.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2023 Red Hat, Inc.
+ *
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  and Apache License v2.0 which accompanies this distribution.
+ *
+ *  The Eclipse Public License is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  The Apache License v2.0 is available at
+ *  http://www.opensource.org/licenses/apache2.0.php
+ *
+ *  You may elect to redistribute this code under either of these licenses.
+ */
+package io.vertx.openapi.contract;
+
+import io.vertx.codegen.annotations.Nullable;
+import io.vertx.codegen.annotations.VertxGen;
+
+import java.util.Set;
+
+/**
+ * Represents an OAuth Flow Object (the configuration).
+ */
+@VertxGen
+public interface OAuthFlow extends OpenAPIObject {
+
+  /**
+   * The authorization URL to be used for this flow. This MUST be in the form of a URL.
+   * The OAuth2 standard requires the use of TLS.
+   */
+  String getAuthorizationUrl();
+
+  /**
+   * The token URL to be used for this flow. This MUST be in the form of a URL.
+   * The OAuth2 standard requires the use of TLS.
+   */
+  String getTokenUrl();
+
+  /**
+   * The URL to be used for obtaining refresh tokens. This MUST be in the form of a URL.
+   * The OAuth2 standard requires the use of TLS.
+   */
+  @Nullable
+  String getRefreshUrl();
+
+  /**
+   * The available scopes for the OAuth2 security scheme. A set of the scope names.
+   * The set MAY be empty.
+   */
+  Set<String> getScopes();
+}
---
+++ src/main/java/io/vertx/openapi/contract/impl/OAuthFlowImpl.java
@@ -0,0 +1,53 @@
+package io.vertx.openapi.contract.impl;
+
+import io.vertx.core.json.JsonObject;
+import io.vertx.openapi.contract.OAuthFlow;
+
+import java.util.Collections;
+import java.util.Set;
+
+public class OAuthFlowImpl implements OAuthFlow {
+
+  private final JsonObject model;
+
+  private final String authorizationUrl;
+  private final String tokenUrl;
+  private final String refreshUrl;
+  private final Set<String> scopes;
+
+
+  public OAuthFlowImpl(JsonObject json) {
+    this.model = json;
+    this.authorizationUrl = json.getString("authorizationUrl");
+    this.tokenUrl = json.getString("tokenUrl");
+    this.refreshUrl = json.getString("refreshUrl");
+    this.scopes = json.containsKey("scopes") ?
+      json.getJsonObject("scopes").fieldNames() :
+      Collections.emptySet();
+  }
+
+  @Override
+  public String getAuthorizationUrl() {
+    return authorizationUrl;
+  }
+
+  @Override
+  public String getTokenUrl() {
+    return tokenUrl;
+  }
+
+  @Override
+  public String getRefreshUrl() {
+    return refreshUrl;
+  }
+
+  @Override
+  public Set<String> getScopes() {
+    return scopes;
+  }
+
+  @Override
+  public JsonObject getOpenAPIModel() {
+    return model;
+  }
+}
