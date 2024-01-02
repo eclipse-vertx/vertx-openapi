609dc403e8ceb919f572745b2ea1789aa3bfbfed
Gerrett <Gerrett.Pape@colorado.edu>
2024-01-01 18:44:42 -0600

    Removing unused imports.
    Signed-off-by: Gerrett <Gerrett.Pape@colorado.edu>

--- src/test/java/io/vertx/openapi/validation/ParameterTest.java
+++ src/test/java/io/vertx/openapi/validation/ParameterTest.java
@@ -20,8 +20,6 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.stream.Stream;
 
 import static com.google.common.truth.Truth.assertThat;
