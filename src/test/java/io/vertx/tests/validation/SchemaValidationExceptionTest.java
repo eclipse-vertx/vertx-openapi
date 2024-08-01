74a1dbdde559046334a3942356bfc6ef1e7f6dac
Gerrett <Gerrett.Pape@colorado.edu>
2024-08-01 17:43:48 -0500

    Adding a new method to throw an exception when the output type is MISSING_VALUE. Adding unit tests.
    Signed-off-by: Gerrett <Gerrett.Pape@colorado.edu>

--- src/test/java/io/vertx/tests/validation/SchemaValidationExceptionTest.java
+++ src/test/java/io/vertx/tests/validation/SchemaValidationExceptionTest.java
@@ -24,6 +24,7 @@ import org.junit.jupiter.api.Test;
 
 import static com.google.common.truth.Truth.assertThat;
 import static io.vertx.json.schema.common.dsl.Schemas.intSchema;
+import static io.vertx.openapi.validation.ValidatorErrorType.MISSING_REQUIRED_PARAMETER;
 import static io.vertx.tests.MockHelper.mockParameter;
 import static io.vertx.openapi.contract.Location.PATH;
 import static io.vertx.openapi.contract.Style.LABEL;
@@ -41,9 +42,13 @@ class SchemaValidationExceptionTest {
   private static final OutputUnit DUMMY_OUTPUT_UNIT = new OutputUnit("instanceLocation2", "absoluteKeywordLocation2",
     "keywordLocation2", "error2", OutputErrorType.MISSING_VALUE);
 
+  private static final OutputUnit DUMMY_OUTPUT_UNIT_INVALID = new OutputUnit("instanceLocation2", "absoluteKeywordLocation2",
+    "keywordLocation2", "error2", OutputErrorType.INVALID_VALUE);
+
   @BeforeAll
   static void setup() {
     DUMMY_OUTPUT_UNIT.setErrors(ImmutableList.of(DUMMY_ERROR_UNIT));
+    DUMMY_OUTPUT_UNIT_INVALID.setErrors(ImmutableList.of(DUMMY_ERROR_UNIT));
   }
 
   @Test
@@ -78,4 +83,40 @@ class SchemaValidationExceptionTest {
     String excpectedMsg = "The value of the response body is invalid. Reason: error at instanceLocation";
     assertThat(exception).hasMessageThat().isEqualTo(excpectedMsg);
   }
+
+  @Test
+  void testCreateMissingValueRequestBody() {
+    SchemaValidationException exception = SchemaValidationException.createMissingValueRequestBody(DUMMY_OUTPUT_UNIT,
+      DUMMY_CAUSE);
+
+    assertThat(exception.getOutputUnit()).isEqualTo(DUMMY_OUTPUT_UNIT);
+    assertThat(exception.getCause()).isEqualTo(DUMMY_CAUSE);
+    assertThat(exception.type()).isEqualTo(MISSING_REQUIRED_PARAMETER);
+    String excpectedMsg = "The value of the request body is missing. Reason: error at instanceLocation";
+    assertThat(exception).hasMessageThat().isEqualTo(excpectedMsg);
+  }
+
+  @Test
+  void testCreateErrorFromOutputUnitType() {
+
+    SchemaValidationException exception = SchemaValidationException.createErrorFromOutputUnitType(DUMMY_PARAMETER,
+      DUMMY_OUTPUT_UNIT, DUMMY_CAUSE);
+
+    assertThat(exception.getOutputUnit()).isEqualTo(DUMMY_OUTPUT_UNIT);
+    assertThat(exception.getCause()).isEqualTo(DUMMY_CAUSE);
+    assertThat(exception.type()).isEqualTo(MISSING_REQUIRED_PARAMETER);
+    String excpectedMsg = "The value of the request body is missing. Reason: error at instanceLocation";
+    assertThat(exception).hasMessageThat().isEqualTo(excpectedMsg);
+
+    SchemaValidationException exception_invalid = SchemaValidationException.createErrorFromOutputUnitType(DUMMY_PARAMETER,
+      DUMMY_OUTPUT_UNIT_INVALID, DUMMY_CAUSE);
+
+    assertThat(exception_invalid.getOutputUnit()).isEqualTo(DUMMY_OUTPUT_UNIT_INVALID);
+    assertThat(exception_invalid.getCause()).isEqualTo(DUMMY_CAUSE);
+    assertThat(exception_invalid.type()).isEqualTo(INVALID_VALUE);
+    excpectedMsg = "The value of path parameter dummy is invalid. Reason: error at instanceLocation";
+    assertThat(exception_invalid).hasMessageThat().isEqualTo(excpectedMsg);
+
+  }
+
 }
