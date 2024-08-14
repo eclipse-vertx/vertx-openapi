de22b471370f65fb86a10a6020a777adc7795ebc
Sebastian Veit <sebastian.veit@lucimber.com>
2024-08-14 23:52:31 +0200

    contract: Add application/octet-stream to supported media types
    Signed-off-by: Sebastian Veit <sebastian.veit@lucimber.com>

--- src/test/java/io/vertx/openapi/contract/impl/ResponseImplTest.java
+++ src/test/java/io/vertx/openapi/contract/impl/ResponseImplTest.java
@@ -68,7 +68,7 @@ class ResponseImplTest {
       Arguments.of("0000_Response_With_Content_Type_Text_Plain", UNSUPPORTED_FEATURE,
         "The passed OpenAPI contract contains a feature that is not supported: Operation dummyOperation defines a " +
           "response with an unsupported media type. Supported: application/json, application/json; charset=utf-8, " +
-          "multipart/form-data, application/hal+json")
+          "multipart/form-data, application/hal+json, application/octet-stream")
     );
   }
 
