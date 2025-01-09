be2c21647deb924e4e989f0b64761c357f4fed0b
Pascal Krause <pascal.krause@sap.com>
2025-01-09 08:45:47 +0100

    fix: missing method generation for Rx
    see https://github.com/vert-x3/vertx-rx/issues/320
    
    Signed-off-by: Pascal Krause <pascal.krause@sap.com>

--- src/main/java/io/vertx/openapi/validation/Parameter.java
+++ src/main/java/io/vertx/openapi/validation/Parameter.java
@@ -14,10 +14,12 @@ package io.vertx.openapi.validation;
 
 import io.vertx.codegen.annotations.CacheReturn;
 import io.vertx.codegen.annotations.Nullable;
+import io.vertx.codegen.annotations.VertxGen;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 
+@VertxGen(concrete = false)
 public interface Parameter {
   /**
    * @return null if value is not a {@link String}, otherwise it returns value
