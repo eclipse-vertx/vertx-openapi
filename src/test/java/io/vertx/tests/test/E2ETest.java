ff91ebabb37ea4d2554b3fd53b3d0601922a1d1b
Pascal Krause <pascal.krause@sap.com>
2025-04-28 17:52:57 +0200

    refactor: add ContractTestBase to simplify creation of new E2E tests
    Signed-off-by: Pascal Krause <pascal.krause@sap.com>

--- src/test/java/io/vertx/tests/test/E2ETest.java
+++ src/test/java/io/vertx/tests/test/E2ETest.java
@@ -14,54 +14,27 @@ package io.vertx.tests.test;
 
 import static com.google.common.truth.Truth.assertThat;
 import static io.vertx.tests.ResourceHelper.getRelatedTestResourcePath;
-import static io.vertx.tests.ResourceHelper.loadJson;
 
 import io.netty.handler.codec.http.HttpHeaderValues;
 import io.vertx.core.Future;
 import io.vertx.core.http.HttpClientRequest;
-import io.vertx.core.http.HttpClientResponse;
 import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.junit5.Timeout;
 import io.vertx.junit5.VertxTestContext;
-import io.vertx.openapi.contract.OpenAPIContract;
-import io.vertx.openapi.validation.RequestValidator;
-import io.vertx.openapi.validation.ResponseValidator;
 import io.vertx.openapi.validation.ValidatableResponse;
-import io.vertx.openapi.validation.ValidatedRequest;
-import io.vertx.tests.test.base.HttpServerTestBase;
+import io.vertx.tests.test.base.ContractTestBase;
 import java.nio.file.Path;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Consumer;
-import java.util.function.Function;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.parallel.Execution;
-import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-// We need to enforce sequential execution, because the tests are manipulating fields
-@Execution(ExecutionMode.SAME_THREAD)
-class E2ETest extends HttpServerTestBase {
-
-  private OpenAPIContract contract;
-  private RequestValidator requestValidator;
-  private ResponseValidator responseValidator;
-
-  Future<Void> setupContract(String basePath, VertxTestContext testContext) {
-    JsonObject contractJson = loadJson(vertx, getRelatedTestResourcePath(E2ETest.class).resolve("petstore.json"));
-    // Modify base path
-    JsonObject server = contractJson.getJsonArray("servers").getJsonObject(0);
-    server.put("url", server.getString("url") + basePath);
-    return OpenAPIContract.from(vertx, contractJson).onComplete(testContext.succeeding(contract -> {
-      this.contract = contract;
-      this.requestValidator = RequestValidator.create(vertx, contract);
-      this.responseValidator = ResponseValidator.create(vertx, contract);
-    })).mapEmpty();
-  }
+class E2ETest extends ContractTestBase {
+  private Path CONTRACT_FILE = getRelatedTestResourcePath(E2ETest.class).resolve("petstore.json");
 
   @Timeout(value = 2, timeUnit = TimeUnit.SECONDS)
   @ParameterizedTest(name = "{index} Test with base path: {0}")
@@ -70,16 +43,16 @@ class E2ETest extends HttpServerTestBase {
     int expectedPetId = 1;
     JsonObject expectedPet = new JsonObject().put("id", 1).put("name", "FooBar");
 
-    setupContract(basePath, testContext).compose(v -> createValidationHandler(req -> {
+    loadContract(CONTRACT_FILE, basePath, testContext).compose(v -> createServerWithRequestProcessor(req -> {
       testContext.verify(() -> {
         int petId = req.getPathParameters().get("petId").getInteger();
         assertThat(petId).isEqualTo(expectedPetId);
       });
       return ValidatableResponse.create(200, expectedPet.toBuffer(), HttpHeaderValues.APPLICATION_JSON.toString());
-    }, contract.operation("showPetById").getOperationId(), testContext)).compose(v -> {
+    }, "showPetById", testContext)).compose(v -> {
       String bp = basePath.endsWith("/") ? basePath.substring(0, basePath.length() - 1) : basePath;
       Future<HttpClientRequest> req = createRequest(HttpMethod.GET, bp + "/pets/" + expectedPetId);
-      return request(req, resp -> resp.body().onComplete(testContext.succeeding(
+      return sendAndVerifyRequest(req, resp -> resp.body().onComplete(testContext.succeeding(
           body -> testContext.verify(() -> {
             assertThat(resp.statusCode()).isEqualTo(200);
             assertThat(body.toJsonObject()).isEqualTo(expectedPet);
@@ -104,7 +77,7 @@ class E2ETest extends HttpServerTestBase {
             .put("email", "example@example.com")
             .put("phone", "5555555555"));
 
-    setupContract("", testContext).compose(v -> createValidationHandler(req -> {
+    loadContract(CONTRACT_FILE, testContext).compose(v -> createServerWithRequestProcessor(req -> {
       testContext.verify(() -> {
         assertThat(req.getBody()).isNotNull();
         JsonObject jsonReq = req.getBody().getJsonObject();
@@ -114,7 +87,7 @@ class E2ETest extends HttpServerTestBase {
         testContext.completeNow();
       });
       return ValidatableResponse.create(201);
-    }, contract.operation("uploadPet").getOperationId(), testContext))
+    }, "uploadPet", testContext))
         .compose(v -> createRequest(HttpMethod.POST, "/pets/upload"))
         .map(request -> request.putHeader(HttpHeaders.CONTENT_TYPE, "multipart/form-data; boundary=4ad8accc990e99c2"))
         .map(request -> request.putHeader(HttpHeaders.CONTENT_DISPOSITION, ""))
@@ -129,28 +102,13 @@ class E2ETest extends HttpServerTestBase {
   void testLessRestrictiveContentType(String requestContentType, VertxTestContext testContext) {
     JsonObject expectedPet = new JsonObject().put("id", 1).put("name", "FooBar");
 
-    setupContract("", testContext).compose(v -> createValidationHandler(req -> {
+    loadContract(CONTRACT_FILE, testContext).compose(v -> createServerWithRequestProcessor(req -> {
       testContext.completeNow();
       return ValidatableResponse.create(201);
-    }, contract.operation("createPets").getOperationId(), testContext))
+    }, "createPets", testContext))
         .compose(v -> createRequest(HttpMethod.POST, "/pets"))
         .map(request -> request.putHeader(HttpHeaders.CONTENT_TYPE, requestContentType))
         .compose(request -> request.send(expectedPet.toBuffer()))
         .onFailure(testContext::failNow);
-  }
-
-  private Future<Void> request(Future<HttpClientRequest> request, Consumer<HttpClientResponse> verifier,
-      VertxTestContext testContext) {
-    return request.compose(HttpClientRequest::send)
-        .onSuccess(response -> testContext.verify(() -> verifier.accept(response))).onFailure(testContext::failNow)
-        .mapEmpty();
-  }
-
-  private Future<Void> createValidationHandler(Function<ValidatedRequest, ValidatableResponse> processor,
-      String operationId, VertxTestContext testContext) {
-    return createServer(request -> requestValidator.validate(request, operationId)
-        .map(processor).compose(validatableResponse -> responseValidator.validate(validatableResponse, operationId))
-        .compose(validatedResponse -> validatedResponse.send(request.response()))
-        .onFailure(testContext::failNow)).onFailure(testContext::failNow);
   }
 }
